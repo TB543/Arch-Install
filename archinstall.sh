cc122de8301266da2e675e228a5beda48c5a850d
Ty <92407605+TB543@users.noreply.github.com>
2026-09-03 17:37:46 -0400

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -5,7 +5,7 @@
 read "key?Enter the user credentials encryption key: "
 read "ans?Edit config? [y/n]: "
 
-if [ "$ans" = "y" ]; then
+if [[ "$ans" = "y" ]]; then
     archinstall --config-url https://raw.githubusercontent.com/TB543/Arch-Install/refs/heads/main/config/user_configuration.json \
         --creds-url https://raw.githubusercontent.com/TB543/Arch-Install/refs/heads/main/config/user_credentials.json \
         --creds-decryption-key "$key" \
