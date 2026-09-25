8b11bd208c9f3acf758a8065568eabae4d89ef17
Ty <92407605+TB543@users.noreply.github.com>
2026-09-25 13:44:46 -0400

    Update hyprland.lua

--- config/hyprland.lua
+++ config/hyprland.lua
@@ -35,7 +35,7 @@ hl.monitor({
 
 -- startup commands
 hl.on("hyprland.start", function () 
-   hl.exec_cmd("caelestia shell -d")
+    hl.exec_cmd("caelestia shell -d")
     hl.exec_cmd("wl-paste --type text --watch cliphist store")
     hl.exec_cmd("wl-paste --type image --watch cliphist store")
     hl.exec_cmd("cliphist wipe")
